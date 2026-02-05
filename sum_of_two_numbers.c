0c2ff7f8c89267c96be4890d5a668e4759d69936
Munsi-Gowsal <sunmunsi8436@gmail.com>
2026-02-05 18:10:51 +0530

    Added sum of two numbers program

---
+++ sum_of_two_numbers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() 
+{
+   int a,b;
+    scanf("%d", &a);
+    scanf("%d", &b);
+     int res = a+b;
+    printf("The sum of %d and %d is %d.\n",a,b,res);
+    
+    return 0;
+}
