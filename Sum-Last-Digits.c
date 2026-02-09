7b9f5a7dc8a8baa2d9aed51bc6e906a1abe813f3
Munsi-Gowsal <sunmunsi8436@gmail.com>
2026-02-10 00:23:45 +0530

    Sum of last digit

---
+++ Sum-Last-Digits.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+ int sumLastDigits(int a, int b){
+     return (a%10)*1+(b%10)*1;
+   
+ }
+int main() {
+      int a,b;
+    scanf("%d %d",&a,&b);
+    int res=sumLastDigits(a,b);
+    printf("The sum of last digits is: %d",res);
+    
+    return 0;
+}
